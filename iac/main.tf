63cf321a2049750de23ba119990a56cbf8d199ea
Gerard González <gerardgonzalez.ai@gmail.com>
2023-10-08 20:20:52 -0300

    Adding terrafon to project

---
+++ iac/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_cloud_run_service" "doubletap_service" {
+  name     = "doubletap-service"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gerardgonzalez7/doubletap:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  autogenerate_revision_name = true
+}
+
+output "service_url" {
+  value     = google_cloud_run_service.doubletap_service.status[0].url
+  sensitive = false
+}
+
+variable "credentials_file" {}
+variable "project_id" {
+  default = "gg-website"
+}
+variable "region" {
+  default = "us-central1"
+}
